a9e09ebebd7e5da9cbc3aca29e11f8ab341139d5
raihan512 <raihangazi1024@gmail.com>
2024-04-29 23:36:50 +0600

    routes changed

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -21,28 +21,32 @@ export const router = createBrowserRouter([
         path: "books",
         element: <AllBooks />,
       },
-    ],
-  },
-  {
-    path: "auth",
-    element: <AuthLayout />,
-    children: [
       {
-        path: "",
-        element: <Auth />,
-      },
-      {
-        path: "signin",
+        path: "auth/signin",
         element: <SignIn />,
       },
-      {
-        path: "signup",
-        element: <SignUp />,
-      },
-      {
-        path: "resetpassword",
-        element: <ResetPassword />,
-      },
     ],
   },
+  // {
+  //   path: "auth",
+  //   element: <AuthLayout />,
+  //   children: [
+  //     {
+  //       path: "",
+  //       element: <Auth />,
+  //     },
+  //     {
+  //       path: "signin",
+  //       element: <SignIn />,
+  //     },
+  //     {
+  //       path: "signup",
+  //       element: <SignUp />,
+  //     },
+  //     {
+  //       path: "resetpassword",
+  //       element: <ResetPassword />,
+  //     },
+  //   ],
+  // },
 ]);

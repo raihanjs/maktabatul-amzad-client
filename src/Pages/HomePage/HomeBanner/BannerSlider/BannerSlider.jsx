9d0f51e83461ce128ed629ee2fe567cd64d6631e
raihan512 <raihangazi1024@gmail.com>
2024-05-21 10:57:52 +0600

    home banner issue solved

--- src/Pages/HomePage/HomeBanner/BannerSlider/BannerSlider.jsx
+++ src/Pages/HomePage/HomeBanner/BannerSlider/BannerSlider.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import { useContext } from "react";
 import { useRef } from "react";
 import Slider from "react-slick";
-import { Link } from "react-router-dom";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import useBanner from "../../../../hooks/useBanner";
+import { ThemeContext } from "../../../../Providers/ThemeProvider";
 
 export default function BannerSlider() {
+  const { language } = useContext(ThemeContext);
+  const [banners, isLoading] = useBanner();
+  const activeBanners = banners.filter((banner) => banner.isActive === true);
   const sliderBtns = useRef(null);
   const settings = {
     dots: false,
@@ -14,71 +18,89 @@ export default function BannerSlider() {
     slidesToScroll: 1,
   };
   return (
-    <div className="md:col-span-8 lg:col-span-4 xl:col-span-3 border relative py-5 px-10 lg:px-20 flex flex-col justify-center">
-      <Slider ref={sliderBtns} {...settings}>
-        {/* Slider item */}
-        <div className="">
-          <div className="flex items-center justify-center">
-            <div className="w-8/12">
-              <h3 className="hero-title text-lg md:text-xl lg:text-2xl xl:text-4xl font-bold line-clamp-2">
-                Interpretation Of The Great Qur'an Tafsir Ibn Kathir I Ibn Hazm{" "}
-              </h3>
-              <p className="text-base lg:text-lg mt-2 mb-4 line-clamp-2">
-                The Author Book Interpretation of the great qur'an tafsir ibn
-                kathir i ibn hazm and the author of 3 another books
-              </p>
-            </div>
-            <div className="w-4/12">
-              <div>
-                <img
-                  src="https://www.noor-book.com/publice/covers_cache_webp/1/d/d/5/02f62b4165dd537df60326da06d3724d.jpg.webp"
-                  className="w-full lg:w-9/12 xl:w-7/12 ml-auto"
-                  alt=""
-                />
-              </div>
-            </div>
+    <div className="border relative py-5 px-10 lg:px-20 flex flex-col justify-center">
+      {isLoading ? (
+        <>
+          <div className="h-screen flex jutify-center items-center">
+            <p className="md:text-xl">Loading Banners ...</p>
           </div>
-        </div>
-        {/* Slider item */}
-        <div className="">
-          <div className="flex items-center justify-center">
-            <div className="w-8/12">
-              <h3 className="hero-title text-lg md:text-xl lg:text-2xl xl:text-4xl font-bold line-clamp-2">
-                Interpretation Of The Great Qur'an Tafsir Ibn Kathir I Ibn Hazm{" "}
-              </h3>
-              <p className="text-base lg:text-lg mt-2 mb-4 line-clamp-2">
-                The Author Book Interpretation of the great qur'an tafsir ibn
-                kathir i ibn hazm and the author of 3 another books
-              </p>
+        </>
+      ) : (
+        <>
+          {activeBanners.length > 1 ? (
+            <>
+              <Slider ref={sliderBtns} {...settings}>
+                {activeBanners.map((banner) => (
+                  <div key={banner?._id}>
+                    <div className="flex items-center justify-center">
+                      <div className="w-8/12">
+                        <h3 className="hero-title text-lg md:text-xl lg:text-2xl xl:text-4xl font-bold line-clamp-2">
+                          {banner?.title[language]}
+                        </h3>
+                        <p className="text-base lg:text-lg mt-2 mb-4 line-clamp-2">
+                          {banner?.text[language]}
+                        </p>
+                      </div>
+                      <div className="w-4/12">
+                        <div>
+                          <img
+                            src={banner?.thumb}
+                            className="w-full lg:w-9/12 xl:w-7/12 ml-auto"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </Slider>
+            </>
+          ) : (
+            <>
+              {activeBanners.map((banner) => (
+                <div key={banner?._id}>
+                  <div className="flex items-center justify-center">
+                    <div className="w-8/12">
+                      <h3 className="hero-title text-lg md:text-xl lg:text-2xl xl:text-4xl font-bold line-clamp-2">
+                        {banner?.title[language]}
+                      </h3>
+                      <p className="text-base lg:text-lg mt-2 mb-4 line-clamp-2">
+                        {banner?.text[language]}
+                      </p>
+                    </div>
+                    <div className="w-4/12">
+                      <div>
+                        <img
+                          src={banner?.thumb}
+                          className="w-full lg:w-9/12 xl:w-7/12 ml-auto"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </>
+          )}
+          {/* Slider buttons */}
+          {activeBanners.length > 1 && (
+            <div className="absolute top-3/5 left-0 w-full px-1 lg:px-2 flex justify-between">
+              <button
+                onClick={() => sliderBtns.current.slickPrev()}
+                className="py-4 px-1 bg-gray-400 hover:bg-[#F0141E]"
+              >
+                <IoIosArrowBack className="text-white text-lg md:text-xl hover:text-black" />
+              </button>
+              <button
+                onClick={() => sliderBtns.current.slickNext()}
+                className="py-4 px-1 bg-gray-400 hover:bg-[#F0141E]"
+              >
+                <IoIosArrowForward className="text-white text-lg md:text-xl hover:text-black" />
+              </button>
             </div>
-            <div className="w-4/12">
-              <div>
-                <img
-                  src="https://www.noor-book.com/publice/covers_cache_webp/1/d/d/5/02f62b4165dd537df60326da06d3724d.jpg.webp"
-                  className="w-full lg:w-9/12 xl:w-7/12 ml-auto"
-                  alt=""
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* Slider item */}
-      </Slider>
-      {/* Slider buttons */}
-      <div className="absolute top-3/5 left-0 w-full px-1 lg:px-2 flex justify-between">
-        <button
-          onClick={() => sliderBtns.current.slickPrev()}
-          className="py-4 px-1 bg-gray-400 hover:bg-[#F0141E]"
-        >
-          <IoIosArrowBack className="text-white text-lg md:text-xl hover:text-black" />
-        </button>
-        <button
-          onClick={() => sliderBtns.current.slickNext()}
-          className="py-4 px-1 bg-gray-400 hover:bg-[#F0141E]"
-        >
-          <IoIosArrowForward className="text-white text-lg md:text-xl hover:text-black" />
-        </button>
-      </div>
+          )}
+        </>
+      )}
     </div>
   );
 }
